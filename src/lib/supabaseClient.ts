5f0a39ca527288c363e4975cead630f6d316282a
Isaac Neiff <isaacneiff254@gmail.com>
2025-09-16 20:11:59 +0000

    Try fixing this error: `Runtime Error: Error: A "use server" file can on

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,5 +1,3 @@
-'use server';
-
 import { createClient } from '@supabase/supabase-js'
 import type { Database } from './types';
 
@@ -7,7 +5,7 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error('Supabase URL and anonymous key are required.');
+  throw new Error('Supabase URL and anonymous key are required. Make sure to set NEXT_PUBLIC_SUPABASE_URL and NEXT_PUBLIC_SUPABASE_ANON_KEY in your .env file.');
 }
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
