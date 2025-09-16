1bace916470225cc234d5d90d200ed301d229db9
Isaac Neiff <isaacneiff254@gmail.com>
2025-09-16 20:18:25 +0000

    Try fixing this error: `Runtime Error: Error: FormDescription is not def

--- src/components/register/RegistrationForm.tsx
+++ src/components/register/RegistrationForm.tsx
@@ -5,7 +5,7 @@ import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from 'zod';
 import { Button } from '@/components/ui/button';
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
